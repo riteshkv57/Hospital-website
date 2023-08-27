a5b768dde9e039bf24ecbff9c3e088cbb29c12cb
Ritesh verma <98600361+riteshkv57@users.noreply.github.com>
2023-08-27 17:49:47 +0530

    Add files via upload

---
+++ Hospital Website/login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission for now
+    // Perform login logic here
+    alert('Logged in successfully!');
+});
